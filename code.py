1383a492ff34e7dcaec6c9b1ac9c377a317d958e
Hannah Alex <hannahalex234@gmail.com>
2025-04-13 15:00:18 +0530

    Initial commit

---
+++ code.py
@@ -0,0 +1,4 @@
+import imageio.v2 as imageio 
+images = ['chicklet1.png', 'chicklet2.png','chicklet3.png','chicklet4.png']
+data = [imageio.imread(img) for img in images]
+imageio.mimsave('team.gif', data, duration=500, loop=0)
